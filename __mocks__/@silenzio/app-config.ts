1e49420cb364df5628dc5b19a84a8a8c5fdfd2bc
michelebruno <43304601+michelebruno@users.noreply.github.com>
2024-01-26 18:14:21 +0100

    boh

--- __mocks__/@silenzio/app-config.ts
+++ __mocks__/@silenzio/app-config.ts
@@ -1,7 +1,7 @@
 const config: Silenzio.Config = {
   // @ts-expect-error - used for testing purposes
   isMock: true,
-  hostname: "https://example.com",
+  hostname: new URL("https://example.com"),
   cache: {
     domains: [new URL("https://example.com")],
     secret: "mock-secret",
